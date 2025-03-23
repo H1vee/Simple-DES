9115d2b931bf2e762559b55231a7353700663f24
H1vee <ailedinskyi@gmail.com>
2025-03-23 17:37:26 +0200

    Implemented CryptoBridge.

--- src/bridge/CryptoBridge.cpp
+++ src/bridge/CryptoBridge.cpp
@@ -3,3 +3,44 @@
 //
 
 #include "CryptoBridge.h"
+
+CryptoBridge::CryptoBridge(QObject *parent):QObject(parent), keyValid(false) {
+
+}
+
+bool CryptoBridge::isKeyValid()const {
+    return keyValid;
+}
+
+QString CryptoBridge::encrypt(const QString& plainText) {
+    if (!keyValid) {
+        emit encryptionCompleted("Error: Key not set");
+        return "Error: Key not set";
+    }
+    const std::string result = sdes.encrypt(plainText.toStdString());
+    QString qResult = QString::fromStdString(result);
+    emit encryptionCompleted(qResult);
+    return qResult;
+}
+
+
+QString CryptoBridge::decrypt(const QString& cipherText) {
+    if (!keyValid) {
+        emit decryptionCompleted("Error: Key not set");
+        return "Error: Key not set";
+    }
+    const std::string result = sdes.decrypt(cipherText.toStdString());
+    QString qResult = QString::fromStdString(result);
+    emit decryptionCompleted(qResult);
+    return qResult;
+}
+
+QString CryptoBridge::textToBinary(const QString& text) {
+    std::string binary = SDES::textToBinary(text.toStdString());
+    return QString::fromStdString(binary);
+}
+
+QString CryptoBridge::binaryToText(const QString& binary) {
+    std::string text = SDES::binaryToText(binary.toStdString());
+    return QString::fromStdString(text);
+}
--- src/bridge/CryptoBridge.h
+++ src/bridge/CryptoBridge.h
@@ -6,10 +6,38 @@
 #define CRYPTOBRIDGE_H
 
 #include <QObject>
+#include <QString>
+#include "../core/SDES.h"
 
+class CryptoBridge: public QObject {
 
-class CryptoBridge {
+    Q_OBJECT
 
+public:
+    explicit CryptoBridge(QObject *parent = nullptr);
+
+public slots:
+    bool setKey(const  QString &key);
+
+    bool isKeyValid() const;
+
+    QString encrypt(const  QString &plainText);
+
+    QString decrypt(const  QString &cipherText);
+
+    QString textToBinary(const  QString &text);
+    QString binaryToText(const QString &binary);
+
+signals:
+
+    void keyChanged(bool valid);
+
+    void encryptionCompleted(const QString& result);
+    void decryptionCompleted(const QString& result);
+
+private:
+    SDES sdes;
+    bool keyValid;
 };
 
 
