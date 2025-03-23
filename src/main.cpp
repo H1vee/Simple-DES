0b89fcc935fe3fab9545337052c7fe769d673e72
H1vee <ailedinskyi@gmail.com>
2025-03-23 12:55:21 +0200

    Implemented basic main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,15 @@
 //
 // Created by shingesa on 20/03/25.
 //
+
+#include <QApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    QQmlApplicationEngine engine;
+
+
+    return app.exec();
+}
